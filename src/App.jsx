e8128f0f37e173c0297bc8f039e182f9e15426a2
DAVID LOPES <davidlopes0208@gmail.com>
2023-05-29 17:35:33 +0200

    First try with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
 import "./App.css"
 import PokemonCard from "./components/Card"
-function App() {
-  const [count, setCount] = useState(0)
+// eslint-disable-next-line no-unused-vars
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+]
 
+function App() {
+  const pokemon = pokemonList[0]
   return (
     <>
-      <PokemonCard />
+      <PokemonCard name={pokemon.name} imgSrc={pokemon.imgSrc} />
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,16 @@
-function PokemonCard() {
-  const { name, imgSrc } = pokemonList[1]
-  return name === "bulbasaur" ? (
+import PropTypes from "prop-types"
+function PokemonCard({ name, imgSrc }) {
+  return (
     <figure>
       <img src={imgSrc} alt={name} />
       <figcaption>{name}</figcaption>
     </figure>
-  ) : (
-    <p>???</p>
   )
 }
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-]
+PokemonCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  imgSrc: PropTypes.string.isRequired,
+}
 
 export default PokemonCard
