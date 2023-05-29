a228b74136733c2b607c7216a94a79148659610d
DAVID LOPES <davidlopes0208@gmail.com>
2023-05-29 13:51:02 +0200

    First Condition

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,24 @@
 function PokemonCard() {
-  return (
+  const { name, imgSrc } = pokemonList[1]
+  return name === "bulbasaur" ? (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
+      <img src={imgSrc} alt={name} />
+      <figcaption>{name}</figcaption>
     </figure>
+  ) : (
+    <p>???</p>
   )
 }
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+]
 
 export default PokemonCard
