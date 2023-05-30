d4ab7899c6adc265f919aefb96c08576673059dc
DAVID LOPES <davidlopes0208@gmail.com>
2023-05-30 08:20:23 +0200

    First State

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css"
 import PokemonCard from "./components/Card"
+import { useState } from "react"
 // eslint-disable-next-line no-unused-vars
 const pokemonList = [
   {
@@ -8,15 +9,43 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ]
-
 function App() {
-  const pokemon = pokemonList[0]
+  const [pokemonIndex, setPokemonIndex] = useState(0)
+  const clickPrev = () => {
+    setPokemonIndex(
+      pokemonIndex === 0 ? pokemonList.length - 1 : pokemonIndex - 1
+    )
+  }
+  const clickNext = () => {
+    setPokemonIndex(pokemonIndex + 1)
+  }
+
+  const currentPokemon = pokemonList[pokemonIndex]
+  const disablePrev = pokemonIndex !== 0
+  const disableNext = pokemonIndex !== pokemonList.length - 1
   return (
     <>
-      <PokemonCard pokemon={pokemon} />
+      <PokemonCard pokemon={currentPokemon} />
+      {disablePrev && <button onClick={clickPrev}>Previus</button>}
+      {disableNext && <button onClick={clickNext}>Next</button>}
     </>
   )
 }
